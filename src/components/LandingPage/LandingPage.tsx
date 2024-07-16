afc3458d8a5636ca06c11807f30af7377b1415a1
Deep Rakesh <deeptank09@gmail.com>
2024-07-16 21:45:26 +0530

    Update LandingPage.tsx

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../LandingPage/LandingPage.css";
 import logo from "/images/logo.svg";
 import money from "/images/landing-page-money.png";
