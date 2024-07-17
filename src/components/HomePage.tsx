31782d79ddd4fb182361d34a5f0ef9dfc2d5a16e
Deep Rakesh <deeptank09@gmail.com>
2024-07-17 16:21:47 +0530

    button placement

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,30 +1,55 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
+import "../App.css";
 
 function HomePage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [goToUserLogin, setGoToUserLogin] = React.useState(false);
-    if(goToUserLogin){
-        return <Navigate to="/UserLogin"/>
-    }
-  return (  
+  if (goToUserLogin) {
+    return <Navigate to="/UserLogin" />;
+  }
+  return (
     <>
-    <div className="background-image">
-      <h1>Homepage</h1>
-      <button onClick={() =>{setGoToUserLogin(true)}}>Go to User Login</button>
-      <button onClick={()=>{
-          navigate('/AdminLogin')
-        }}>Admin Login</button>
-        <br /><br />
-      <button onClick={()=>{
-          navigate('/DummyData')
-        }}>Dummy Data Dsiplay</button>
-      <button onClick={()=>{
-          navigate('/LandingPage')
-        }}>Landing Page</button>
+      <div className="background-image">
+        <h1>Homepage</h1>
+        <div className="button-center">
+          <button
+            className="button-highlight poppins-medium"
+            onClick={() => {
+              navigate("/LandingPage");
+            }}
+          >
+            Landing Page
+          </button>
+        </div>
         <br />
-    </div>
-      
+        <br />
+        <button
+          onClick={() => {
+            setGoToUserLogin(true);
+          }}
+        >
+          Go to User Login
+        </button>
+        <button
+          onClick={() => {
+            navigate("/AdminLogin");
+          }}
+        >
+          Admin Login
+        </button>
+        <br />
+        <br />
+        <button
+          onClick={() => {
+            navigate("/DummyData");
+          }}
+        >
+          Dummy Data Dsiplay
+        </button>
+
+        <br />
+      </div>
     </>
   );
 }
